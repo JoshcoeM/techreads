3f96fc618b7bd91c9547bba8f4218b3e06754629
Josh Moscoe <jmoscoe@manifestcorp.com>
2021-11-08 16:34:57 -0500

    Techreads completed from slides

--- src/main/java/com/manifestcorp/techreads/controller/BookController.java
+++ src/main/java/com/manifestcorp/techreads/controller/BookController.java
@@ -4,11 +4,15 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.manifestcorp.techreads.model.Book;
 import com.manifestcorp.techreads.repository.BookRepository;
+import org.springframework.web.servlet.view.RedirectView;
+
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 @Controller
 @RequestMapping("/books")
@@ -24,4 +28,19 @@ public class BookController {
         mav.addObject("books",books);
         return mav;
     }
+
+    @RequestMapping("/add")
+    public String add(Model model){
+        model.addAttribute("bookForm", new Book());
+        return "add";
+    }
+
+    @RequestMapping(value ={"","/"}, method=POST)
+    public RedirectView addBook(Book book){
+        bookRepository.saveAndFlush(book);
+        return new RedirectView("books");
+    }
+
+
+
 }
